ac60025c72bbe5b72469400a57385611ded5df19
Bruce Conner <bruce.conner.ii@gmail.com>
2017-11-26 17:00:22 -0500

    Functioning Game
    This is now a not pretty but functioning game of rock paper scissors of
    the the player vs a randomized computer.

--- roshambo.js
+++ roshambo.js
@@ -6,48 +6,42 @@ var currentBoard = [[2,0,2],
 function cleanGame() {
 	$("#player-throw-display").removeClass();
 	$("#computer-throw-display").removeClass();
+	$(".outcome-display").text();
 }
 
 function processGame(playerThrow) {
 	var computerThrow = determineComputerThrow();
 
 	switch(playerThrow) {
-		case 0:
-		case "0":
-			//throw rock
+		case "rock":
 			$("#player-throw-display").addClass("throw-rock");
 			break;
-		case 1:
-		case "1":
-			//throw paper
+		case "rock":
 			$("#player-throw-display").addClass("throw-paper");
 			break;
-		case 2:
-		case "2":
-			//throw scissors
+		case "scissors":
 			$("#player-throw-display").addClass("throw-scissors");
 			break;
 		default:
 			//the game broke
 	}
 
-	switch(cpuThought) {
-		case 0:
-			//throw rock
+	switch(computerThrow) {
+		case "rock":
 			$("#computer-throw-display").addClass("throw-rock");
 			break;
-		case 1:
-			//throw paper
+		case "rock":
 			$("#computer-throw-display").addClass("throw-paper");
 			break;
-		case 2:
-			//throw scissors
+		case "scissors":
 			$("#computer-throw-display").addClass("throw-scissors");
 			break;
 		default:
-			//throw rock as a safety fallback
+			//the game broke
 	}
 
+	var playerWin = determineWinner(playerThrow, computerThrow);
+	displayGameResults(playerWin);
 }
 
 function gameAnimation() {
@@ -72,28 +66,69 @@ function gameAnimation() {
 }
 
 function determineComputerThrow() {
-	return cpuThought = (Math.floor(Math.random() * 3));
+	var cpuThought = (Math.floor(Math.random() * 3));
 
 	switch(cpuThought) {
 		case 0:
-			//throw rock
+			return "rock";
 			break;
 		case 1:
-			//throw paper
+			return "paper";
 			break;
 		case 2:
-			//throw scissors
+			return "scissors";
 			break;
 		default:
-			//throw rock as a safety fallback
+			return "rock";
 	}	
 
 }
 
-function playerClickHandler() {
-	cleanGame();
+function determineWinner(playerThrow, computerThrow) {
+	if (playerThrow === "rock") {
+		if (computerThrow === "rock") {
+			return "tie";
+		} else if (computerThrow === "paper") {
+			return false;
+		} else if (computerThrow === "scissors") {
+			return true;
+		}
+	}
 
+	if (playerThrow === "paper") {
+		if (computerThrow === "rock") {
+			return true;
+		} else if (computerThrow === "paper") {
+			return "tie";
+		} else if (computerThrow === "scissors") {
+			return false;
+		}
+	}
+
+	if (playerThrow === "scissors") {
+		if (computerThrow === "rock") {
+			return false;
+		} else if (computerThrow === "paper") {
+			return true;
+		} else if (computerThrow === "scissors") {
+			return "tie";
+		}
+	}
+}
+
+function displayGameResults(playerWin) {
+	if (playerWin) {
+		$(".outcome-display").text("YOU WIN!");
+	} else if (playerWin === "tie") {
+		$(".outcome-display").text("TIE!");
+	} else {
+		$(".outcome-display").text("YOU LOSE!");
+	}
+}
+
+function playerClickHandler() {
 	$(".player-option").on("click", function() {
+		cleanGame();
 		//gameAnimation();
 		processGame($(this).attr("data-throw"));
 	});
